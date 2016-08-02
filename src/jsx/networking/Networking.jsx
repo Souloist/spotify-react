96f4b85635a0f48841c0518486bab6baa9cd280b
souloist <rich.shen@nyu.edu>
2016-08-02 18:11:21 -0400

    added album component

--- src/jsx/networking/Networking.jsx
+++ src/jsx/networking/Networking.jsx
@@ -1,11 +1,11 @@
-import axios fomr "axios";
+import axios from "axios";
 
 /**
  * Fetch takes a request to send, and a callback to pass the response to.
  * The request is passed to the axios get method which returns a promise.
  * Once the promise resolves, we call our callback.
  * Axois stores the resulting payload in the response's data object
- *//
+ */
 
 var fetch = function(request, callback) {
     axios.get(request)
--- src/jsx/utils/SpotifyUtils.jsx
+++ src/jsx/utils/SpotifyUtils.jsx
@@ -1,11 +1,13 @@
 import Networking from "../networking/Networking";
 
-export function getAlbums(artist, callback) {
+module.exports = {
+    getAlbums: function(artist, callback) {
     const request = "https://api.spotify.com/v1/search?q=${artist}&type=album";
     Networking.fetch(request, callback);
-}
+    },
 
-export function getTracks(albumId, callback) {
+    getTracks: function(albumId, callback) {
     const request = "https://api.spotify.com/v1/albums/${albumId}";
     Networking.fetch(request, callback);
-}
+    }
+};
