52a4e05d7de123f89c756e46949fc896f39421c6
souloist <rich.shen@nyu.edu>
2017-05-14 19:33:38 -0400

    Remove build folder and update webpack to populate build folder

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ var path = require("path");
 
 var PATHS = {
     "app": "./src/jsx/App.jsx",
+    "html": "/src/index.html",
+    "css": "/src/css/",
     "dist": path.join(__dirname, "build")
 };
 
@@ -14,7 +16,7 @@ var loaders = ["react-hot"]
 
 loaders.push("babel-loader")
 
-module.exports = {
+var config = {
     "entry": {
         "javascript": PATHS.app
     },
@@ -25,22 +27,28 @@ module.exports = {
     },
     "plugins": plugins,
     "devServer": {
+        "open": true, // open local server in browser
         "contentBase": PATHS.dist
     },
     "eslint": {
         "emitWarning": true
     },
     "module": {
-        "preLoaders": [
+        "rules": [
             {
+                "enforce": "pre",
                 "test": /\.(js|jsx)$/,
                 "loaders": ["eslint-loader"],
-                "exclude": /node_modules/
+                "exclude": /(node_modules)/
             }
         ],
         "loaders": [
             {
                 "test": /\.html$/,
+                "loader": "file?name=[name].[ext]"
+            },
+            {
+                "test": /\.css$/,
                 "loader": "file?name=[name].[ext]"
             },
             {
@@ -54,3 +62,5 @@ module.exports = {
         "extensions": ["", ".js", ".jsx"]
     }
 };
+
+module.exports = config
